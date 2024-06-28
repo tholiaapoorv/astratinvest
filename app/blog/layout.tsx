f9f871541ade5718f6c2e21297dc7b23d04a0453
Shubham Sharma <shubham03901@gmail.com>
2024-06-29 01:17:37 +0530

    Converted /blogs to client component

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -7,19 +7,19 @@ import React, { Suspense } from "react";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div className="relative overflow-x-clip mb-12">
+      <div className="relative mb-12 overflow-x-clip">
         <NavBar />
         <BlogLayout />
-        <div className=" w-full flex justify-center items-center mt-12">
-          <div className="bg-[#ECF0FB] w-[95%] rounded-[2rem] h-full phone:py-[2.5rem] smLaptop:py-[5rem] flex flex-col justify-center items-center phone:gap-[2rem]  smLaptop:gap-[4.5rem]">
-            <div className="flex flex-col justify-center items-center gap-7">
-              <p className="text-center font-poppins phone:text-[min(6vh,6vw)] smTablet:text-[min(4.5vh,4.5vw)] font-extralight">
+        <div className="mt-12 flex w-full items-center justify-center">
+          <div className="flex h-full w-[95%] flex-col items-center justify-center rounded-[2rem] bg-[#ECF0FB] phone:gap-[2rem] phone:py-[2.5rem] smLaptop:gap-[4.5rem] smLaptop:py-[5rem]">
+            <div className="flex flex-col items-center justify-center gap-7">
+              <p className="text-center font-poppins font-extralight phone:text-[min(6vh,6vw)] smTablet:text-[min(4.5vh,4.5vw)]">
                 Dive Deep Into{" "}
-                <span className=" text-[#3959E5] font-ivy_thin_italic italic">
+                <span className="font-ivy_thin_italic italic text-[#3959E5]">
                   Quant.
                 </span>
               </p>
-              <p className="laptop:w-[65%]  phone:w-[80%] text-center font-poppins font-light tracking-wider phone:text-[min(3vh,3vw)] smTablet:text-[min(1.8vh,1.8vw)] text-pretty leading-relaxed text-[#000000]">
+              <p className="text-pretty text-center font-poppins font-light leading-relaxed tracking-wider text-[#000000] phone:w-[80%] phone:text-[min(3vh,3vw)] smTablet:text-[min(1.8vh,1.8vw)] laptop:w-[65%]">
                 Designed to equip financial advisors with the precision and
                 agility essential in an ever-evolving financial landscape, the
                 Comparator empowers you with the insights to thrive.
@@ -27,10 +27,11 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             </div>
             <Suspense
               fallback={
-                <div className="w-full h-full flex justify-center items-center">
-                  <Loader2 className="animate-spin w-10 h-auto text-[#000121]" />
+                <div className="flex h-full w-full items-center justify-center">
+                  <Loader2 className="h-auto w-10 animate-spin text-[#000121]" />
                 </div>
-              }>
+              }
+            >
               {children}
             </Suspense>
           </div>
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,41 +1,45 @@
+"use client";
 import BlogCard from "@/components/ui/BlogCard";
 import { blogs } from "@/types";
 import axios from "axios";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Page = async () => {
-  try {
-    const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_APP_URL}/api/blogs`,
-    );
-    const blogs: blogs[] = response.data;
+const Page = () => {
+  const [blogs, setBlogs] = useState<blogs[]>([]);
+  useEffect(() => {
+    const response = axios.get(`${process.env.NEXT_PUBLIC_APP_URL}/api/blogs`);
+    response
+      .then((data) => {
+        setBlogs(data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    return (
-      <div className="grid w-[90%] text-[#000121] phone:grid-cols-1 phone:gap-[2rem] tablet:grid-cols-2 tablet:gap-[2rem] smLaptop:gap-[2rem]">
-        {blogs &&
-          blogs.length !== 0 &&
-          blogs.map((blog, idx) => {
-            return (
-              <Link
-                href={`${process.env.NEXT_PUBLIC_APP_URL}/viewBlog/${blog.slug.current}`}
-                key={idx}
-                className="flex items-center justify-center rounded-[2rem] p-4 transition hover:scale-105"
-              >
-                <BlogCard
-                  title={blog.title}
-                  date={blog.publishedAt}
-                  description={blog.description}
-                  imageSrc={blog.mainImage}
-                />
-              </Link>
-            );
-          })}
-      </div>
-    );
-  } catch (err) {
-    console.log("Blog Page Error ==== ", err);
-  }
+  return (
+    <div className="grid w-[90%] text-[#000121] phone:grid-cols-1 phone:gap-[2rem] tablet:grid-cols-2 tablet:gap-[2rem] smLaptop:gap-[2rem]">
+      {blogs &&
+        blogs.length !== 0 &&
+        blogs.map((blog, idx) => {
+          return (
+            <Link
+              href={`${process.env.NEXT_PUBLIC_APP_URL}/viewBlog/${blog.slug.current}`}
+              key={idx}
+              className="flex items-center justify-center rounded-[2rem] p-4 transition hover:scale-105"
+            >
+              <BlogCard
+                title={blog.title}
+                date={blog.publishedAt}
+                description={blog.description}
+                imageSrc={blog.mainImage}
+              />
+            </Link>
+          );
+        })}
+    </div>
+  );
 };
 
 export default Page;
