c051f9ac3802d1ce6cd20f776bb49fa0af17d887
tholiaapoorv <tholiaapoorv@gmail.com>
2025-01-20 13:20:25 +0530

    remove nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -165,7 +165,6 @@ export default function Home() {
         <KnowAboutYourStocks />
         <Footer />
       </div> */}
-      <NavBar />
        <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
         <h1 style={{color: "white", fontSize: 100, fontFamily: "poppins"}}> We will be live soon </h1>
        </div>
