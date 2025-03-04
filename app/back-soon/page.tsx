b87db2a89079049ec9800cb87401abc47db0f802
tholiaapoorv <tholiaapoorv@gmail.com>
2025-03-05 01:52:57 +0530

    Update page.tsx - ' issues

--- app/back-soon/page.tsx
+++ app/back-soon/page.tsx
@@ -2,7 +2,7 @@ export default function BackSoon() {
  return (
   <div className="flex items-center justify-center min-h-screen bg-blue-600">
    <div className="text-center text-white px-6">
-    <h1 className="text-5xl font-bold">We'll Be Back Soon!</h1>
+    <h1 className="text-5xl font-bold">We&apos;ll Be Back Soon!</h1>
     <p className="text-lg mt-4 max-w-lg mx-auto">
      Our website is currently undergoing maintenance.
     </p>
