a84b9221a8ca1a5bd841d5d11457b7b0fcf724c0
tholiaapoorv <tholiaapoorv@gmail.com>
2025-04-21 12:47:30 +0530

    Update to get back live - test

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,55 +7,6 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 const ivy = localFonts({
-  src: [
-    {
-      path: "../fonts/IvyMode-Regular.ttf",
-    },
-  ],
-  variable: "--ivy",
-});
-
-const ivy_thin = localFonts({
-  src: [
-    {
-      path: "../fonts/IvyMode-ThinItalic.ttf",
-    },
-  ],
-  variable: "--ivy-thin",
-});
-
-export const metadata: Metadata = {
-  title: "AStratinvest",
-  description: "Unlock Opportunities with Quant",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${inter.className} ${ivy.variable} ${ivy_thin.variable}`}
-      >
-        {children}
-        <Toaster closeButton richColors />
-      </body>
-    </html>
-  );
-}
-next
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import localFonts from "next/font/local";
-import { Toaster } from "sonner";
-import { redirect } from "next/navigation";
-
-const inter = Inter({ subsets: ["latin"] });
-
-const ivy = localFonts({
   src: [{ path: "../fonts/IvyMode-Regular.ttf" }],
   variable: "--ivy",
 });
@@ -71,9 +22,6 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  // Redirect all pages to "/back-soon"
-  redirect("/back-soon");
-
   return (
     <html lang="en">
       <body className={`${inter.className} ${ivy.variable} ${ivy_thin.variable}`}>
@@ -83,6 +31,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   );
 }
+
 
 // "use client"; // Required for useRouter()
 
