53d86c1cd09b5ae03f4ff576a91727ffd1973bab
tholiaapoorv <tholiaapoorv@gmail.com>
2025-04-25 17:49:25 +0530

    Update NavBar.tsx
    added AIF on the main page

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -21,8 +21,8 @@ import {
 } from "@/components/ui/sheet";
 
 import Link from "next/link";
-
 import { Separator } from "./ui/separator";
+
 const NavBar = () => {
   const containerMain = useRef<HTMLDivElement>(null);
 
@@ -30,23 +30,27 @@ const NavBar = () => {
     <div ref={containerMain} className="fixed top-0 z-[50] w-full">
       <div className="navbar-wrapper flex h-fit items-center justify-center border-b border-white/10 bg-[#000121] backdrop-blur-lg">
         <div className="navbar-container flex w-[80%] items-center justify-between py-2">
-          <Link
-              href={`${process.env.NEXT_PUBLIC_APP_URL}/AIF`}
-              className="flex items-center justify-center gap-2 font-poppins tracking-wide text-white transition hover:text-[#3959E5] xsPhone:hidden smLaptop:flex"
-            >
-              AIF
-            </Link>
+          
+          {/* Logo */}
           <Link
             href={`${process.env.NEXT_PUBLIC_APP_URL}`}
             className="flex items-center justify-center gap-3"
           >
             <Image src={logo3} alt={""} className="h-auto w-[3.5rem]" />
-            {/* <Image src={logo} alt={""} className="w-[15rem] h-auto" /> */}
             <p className="font-ivy text-[min(3vw,3vh)] tracking-widest text-white">
               ASTRATINVEST
             </p>
           </Link>
+
+          {/* Main Links */}
           <div className="group flex h-full cursor-pointer items-center justify-center gap-10 px-6 pr-0 transition-all">
+            <Link
+              href={`${process.env.NEXT_PUBLIC_APP_URL}/AIF`}
+              className="flex items-center justify-center gap-2 font-poppins tracking-wide text-white transition hover:text-[#3959E5] xsPhone:hidden smLaptop:flex"
+            >
+              AIF
+            </Link>
+
             <Link
               href={`${process.env.NEXT_PUBLIC_APP_URL}/research/quantitative-model`}
               className="flex items-center justify-center gap-2 font-poppins tracking-wide text-white transition hover:text-[#3959E5] xsPhone:hidden smLaptop:flex"
@@ -60,24 +64,38 @@ const NavBar = () => {
             >
               Blogs
             </Link>
+
             <Link
               href={`${process.env.NEXT_PUBLIC_APP_URL}/about-us`}
               className="flex items-center justify-center gap-2 font-poppins tracking-wide text-white transition hover:text-[#3959E5] xsPhone:hidden smLaptop:flex"
             >
               About Us
             </Link>
+
+            {/* Mobile Menu */}
             <Sheet>
               <SheetTrigger>
                 <MenuIcon className="h-auto w-[2.5rem] text-white transition-all hover:text-[#3959E5]" />
               </SheetTrigger>
-              <SheetContent className="realtive z-[200] border-0 bg-[#000121]/70 text-white">
+
+              <SheetContent className="relative z-[200] border-0 bg-[#000121]/70 text-white">
                 <SheetHeader className="font-ivy">
                   <SheetTitle className="text-start text-white xsPhone:mt-5 xsPhone:text-[min(7vh,7vw)] tablet:mt-10 tablet:text-[min(4vh,4vw)]">
                     Menu
                   </SheetTitle>
                 </SheetHeader>
+
                 <Separator className="xsPhone:mt-10 tablet:mt-10" />
+
+                {/* Mobile Menu Links */}
                 <div className="mt-[2rem] flex flex-col gap-4">
+                  <Link
+                    href={`${process.env.NEXT_PUBLIC_APP_URL}/AIF`}
+                    className="font-poppins tracking-wider transition hover:text-[#3959E5] xsPhone:text-[min(6vw,6vh)] tablet:text-[min(3vw,3vh)]"
+                  >
+                    AIF
+                  </Link>
+
                   <Link
                     href={`${process.env.NEXT_PUBLIC_APP_URL}/research/quantitative-model`}
                     className="font-poppins tracking-wider transition hover:text-[#3959E5] xsPhone:text-[min(6vw,6vh)] tablet:text-[min(3vw,3vh)]"
@@ -91,6 +109,7 @@ const NavBar = () => {
                   >
                     Blogs
                   </Link>
+
                   <Link
                     href={`${process.env.NEXT_PUBLIC_APP_URL}/about-us`}
                     className="font-poppins tracking-wider transition hover:text-[#3959E5] xsPhone:text-[min(6vw,6vh)] tablet:text-[min(3vw,3vh)]"
@@ -100,6 +119,7 @@ const NavBar = () => {
                 </div>
               </SheetContent>
             </Sheet>
+
           </div>
         </div>
       </div>
