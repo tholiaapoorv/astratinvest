82166503baf32cffe092bfa6eabe5fba39727b1d
tholiaapoorv <tholiaapoorv@gmail.com>
2025-04-26 14:55:26 +0530

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,9 +1,15 @@
 "use client";
+import { motion } from "framer-motion";
 import Link from "next/link";
 
 const Banner = () => {
   return (
-    <div className="w-full bg-white text-center py-3 text-black font-poppins text-sm sm:text-base tracking-wide shadow">
+    <motion.div
+      initial={{ opacity: 0, y: -50 }} // starts 50px above and invisible
+      animate={{ opacity: 1, y: 0 }} // animates to normal position
+      transition={{ duration: 0.8, ease: "easeOut" }} // smooth timing
+      className="w-full mt-[4rem] bg-white text-center py-3 text-black font-poppins text-sm sm:text-base tracking-wide shadow"
+    >
       We&apos;ve just launched our Category III AIF!&nbsp;
       <Link
         href="/AIF"
@@ -12,7 +18,7 @@ const Banner = () => {
         Click here
       </Link>
       &nbsp;to know more.
-    </div>
+    </motion.div>
   );
 };
 
