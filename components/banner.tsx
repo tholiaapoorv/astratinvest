4370d82171215e3232e513c5cfd894e736e60585
tholiaapoorv <tholiaapoorv@gmail.com>
2025-04-26 14:45:03 +0530

    Create banner.tsx

---
+++ components/banner.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Link from "next/link";
+
+const Banner = () => {
+  return (
+    <div className="w-full bg-white text-center py-3 text-black font-poppins text-sm sm:text-base tracking-wide shadow">
+      We've just launched our Category III AIF!&nbsp;
+      <Link
+        href="/AIF"
+        className="text-blue-600 underline hover:text-blue-800"
+      >
+        Click here
+      </Link>
+      &nbsp;to know more.
+    </div>
+  );
+};
+
+export default Banner;
