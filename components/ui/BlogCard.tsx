37b7bafe0942f35a8d36d74202f0b557cfda1437
tholiaapoorv <tholiaapoorv@gmail.com>
2025-07-07 16:32:47 +0530

    Update BlogCard.tsx

--- components/ui/BlogCard.tsx
+++ components/ui/BlogCard.tsx
@@ -22,7 +22,7 @@ const BlogCard = ({
       <div className="flex h-full flex-col items-start justify-between gap-5">
         <SanityImage src={imageSrc} className="h-auto w-fit rounded-3xl" />
         <div className="flex w-full justify-between font-poppins font-extralight phone:text-[min(3vh,3vw)] smTablet:text-[min(1.6vh,1.6vw)]">
-          <p className=" ">Author: AStratinvest </p> &nbsp; &nbsp; &nbsp;
+          <p className=" ">Author: Astratinvest </p> &nbsp; &nbsp; &nbsp;
           <p className=" ">{new Date(date).toUTCString()}</p>
         </div>
         <p className="font-poppins font-semibold tracking-wide phone:text-[min(4.5vh,4.5vw)] smTablet:text-[min(2.8vw,2.8vh)] smLaptop:text-[min(2.5vh,2.5vw)]">
